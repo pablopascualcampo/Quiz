0b78d5c2d89bf0c91008c3bdc082ca05aa74ddee
pablopascualcampo <pablo.pascual.campo@alumnos.upm.es>
2015-04-28 22:02:18 +0200

    Express partials

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
+var partials = require('express-partials');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+app.use(partials());
+
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
