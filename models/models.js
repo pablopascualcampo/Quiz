e57bf5e71eb9c84e9ef192645139ae9fe5a17186
pablopascualcampo <pablo.pascual.campo@alumnos.upm.es>
2015-05-12 20:02:16 +0200

    Correccion errores

--- models/models.js
+++ models/models.js
@@ -1,22 +1,45 @@
 var path = require('path');
 
+//Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+//SQLite   DATABASE_URL = sqLite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name		= (url[6] || null);
+var user		= (url[2] || null);
+var pwd 		= (url[3] || null);
+var protocol	= (url[1] || null);
+var dialect 	= (url[1] || null);
+var port 		= (url[5] || null);
+var host 		= (url[4] || null);
+var storage		= process.env.DATABASE_STORAGE;
+
 //Cargar el modelo ORM
 var Sequelize = require('sequelize');
 
-//Usar BBDD SQLite
-var sequelize = new Sequelize(null, null, null, { dialect: "sqlite", storage: "quiz.sqlite"});
+//Usar BBDD SQLite o Postgress
+
+var sequelize = new Sequelize(DB_name, user,pwd,
+	{ dialect:  protocol,
+	  protocol: protocol,
+	  port: 	port,
+	  host: 	host,
+	  storage: 	storage,	//Solo SQLite (.env)
+	  omitNull: true		//Solo Postgres
+	}
+
+	);
 
 //Importar la definicion de la tabla 'Quiz' en quiz.js
-var Quiz = sequelize.import(path.join(_dirname, 'quiz'));
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz; //Exportar la definición de la tabla Quiz
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function(){
-	//success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function(count){
+sequelize.sync().then(function(){
+	//then(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().then(function(count){
 		if(count === 0){//La tabla se inicializa sólo si está vacía
-			Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma'}).success(function(){console.log('Base de datos inicializada')});
+			Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma'}).then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
