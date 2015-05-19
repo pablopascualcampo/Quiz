1954aa9201144ff860d4bb0fef610283e0ed190e
pablopascualcampo <pablo.pascual.campo@alumnos.upm.es>
2015-05-19 18:56:15 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);	//autoload :quizId
+
 //Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/quizes/autores', quizController.autores);
-router.get('/quizes/busqueda', quizController.busqueda);
 
 module.exports = router;
