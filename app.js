eddab3c2b420f73bbe15e885383aab194b8ad245
pablopascualcampo <pablo.pascual.campo@alumnos.upm.es>
2015-05-20 09:49:27 +0200

    Editar preguntas

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var partials = require('express-partials');
+var methodOverride = require('method-override');
+
 var routes = require('./routes/index');
 
-var partials = require('express-partials');
 
 var app = express();
 
@@ -22,6 +24,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -49,7 +49,7 @@ exports.index = function(req, res){
 //GET /quizes/:id
 exports.show = function(req, res){
 	res.render('quizes/show', { quiz: req.quiz, errors: []});
-};
+};	//req.quiz: instancia de quiz cargada con autoload
 
 //GET /quizes/:id/answer
 exports.answer = function(req, res){	
@@ -88,4 +88,28 @@ exports.create = function(req, res){
 			}
 		}
 	);
+};
+
+//GET /quizes/:id/edit
+exports.edit = function(req, res){
+	var quiz = req.quiz;	//req.quiz: autoload de instancia quiz
+
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+//PUT /quizes/:id
+exports.update = function(req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz.validate().then(function(err){
+		if(err){
+			res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+		}else{
+			req.quiz.save( {fields: ["pregunta", "respuesta"]})	//Save: Guarda campos pregunta y respuesta en BD
+			.then(function(){
+				res.redirect('/quizes');	//Redireccion HTTP a lista de preguntas (URL relativo)
+			});
+		}
+	});
 };
