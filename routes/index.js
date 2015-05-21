df3cee36b94feeeff2e8f4136161a91babfc1bd4
pablopascualcampo <pablo.pascual.campo@alumnos.upm.es>
2015-05-21 15:02:32 +0200

    Users en DB final

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 //Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
-router.get('/quizes/:quizId(\\d+)/comments/:commentId(//d+)/publish', sessionController.loginRequired, commentController.publish);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 //Definición de ruta de autores
 router.get('/author', author.author);
